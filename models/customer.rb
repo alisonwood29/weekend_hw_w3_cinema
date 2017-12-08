8959011404a002145a7571a9203e4ce279089fe7
Alison Wood <alisonwood29@gmail.com>
2017-12-08 11:06:47 +0000

    created customer, film and ticket classes

--- models/customer.rb
+++ models/customer.rb
@@ -1,7 +1,10 @@
 class Customer
 
+attr_reader :id
+attr_accessor :name, :funds
+
   def initialize(options)
-    @id = options["id"] if options["id"]
+    @id = options["id"].to_i if options["id"]
     @name = options["name"]
     @funds = options["funds"]
   end
