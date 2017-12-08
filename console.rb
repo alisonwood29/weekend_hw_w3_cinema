f2aa425bbaf26062174c9b24def1b826543be155
Alison Wood <alisonwood29@gmail.com>
2017-12-08 14:20:08 +0000

    added CRUD methods for ticket class

--- console.rb
+++ console.rb
@@ -1,7 +1,9 @@
 require("pry-byebug")
 require_relative("./models/customer")
 require_relative("./models/film")
+require_relative("./models/ticket")
 
+Ticket.delete_all()
 Customer.delete_all()
 Film.delete_all()
 
@@ -38,16 +40,48 @@ film_3 = Film.new({
   "price" => "6"
   })
 
-  film_1.save()
-  film_2.save()
-  film_3.save()
+film_1.save()
+film_2.save()
+film_3.save()
 
-  film_1.price = "10"
-  film_1.update()
+film_1.price = "10"
+film_1.update()
+
+
+ticket_1 = Ticket.new({
+  "customer_id" => customer_1.id,
+  "film_id" => film_1.id
+  })
+ticket_2 = Ticket.new({
+  "customer_id" => customer_2.id,
+  "film_id" => film_1.id
+  })
+ticket_3 = Ticket.new({
+  "customer_id" => customer_3.id,
+  "film_id" => film_2.id
+  })
+ticket_4 = Ticket.new({
+  "customer_id" => customer_1.id,
+  "film_id" => film_2.id
+  })
+ticket_5 = Ticket.new({
+  "customer_id" => customer_2.id,
+  "film_id" => film_3.id
+  })
+
+ticket_1.save()
+ticket_2.save()
+ticket_3.save()
+ticket_4.save()
+ticket_5.save()
+
+ticket_5.customer_id = customer_3.id
+ticket_5.update()
 
 
 customers = Customer.all()
 films = Film.all()
+tickets = Ticket.all()
 
 binding.pry
 nil
--- models/ticket.rb
+++ models/ticket.rb
@@ -9,5 +9,33 @@ def initialize(options)
   @film_id = options["film_id"]
 end
 
+def save()
+  sql = "INSERT INTO tickets (customer_id, film_id)
+        VALUES ($1, $2)
+        RETURNING id"
+  values = [@customer_id, @film_id]
+  result = SqlRunner.run(sql, values)
+  @id = result[0]["id"].to_i
+end
+
+def update()
+  sql = "UPDATE tickets
+        SET (customer_id, film_id) = ($1, $2)
+        WHERE id = $3 "
+  values = [@customer_id, @film_id, @id]
+  SqlRunner.run(sql, values)
+end
+
+def Ticket.all()
+  sql = "SELECT * FROM tickets"
+  ticket_hashes = SqlRunner.run(sql)
+  return ticket_hashes.map{|ticket_hash| Ticket.new(ticket_hash)}
+end
+
+def Ticket.delete_all()
+  sql = "DELETE FROM tickets"
+  SqlRunner.run(sql)
+end
+
 
 end
