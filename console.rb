898f41d7dfd63a002716de43d13312bcd61c6a7a
Alison Wood <alisonwood29@gmail.com>
2017-12-08 13:51:22 +0000

    added methods for save and select all in films class

--- console.rb
+++ console.rb
@@ -1,5 +1,6 @@
 require("pry-byebug")
 require_relative("./models/customer")
+require_relative("./models/film")
 
 Customer.delete_all()
 
@@ -23,7 +24,26 @@ customer_3.save()
 customer_1.name = "Alison Wood"
 customer_1.update()
 
+film_1 = Film.new({
+  "title" => "Love Actually",
+  "price" => "8"
+  })
+film_2 = Film.new({
+  "title" => "Polar Express",
+  "price" => "5"
+  })
+film_3 = Film.new({
+  "title" => "It's a Wonderful Life",
+  "price" => "6"
+  })
+
+  film_1.save()
+  film_2.save()
+  film_3.save()
+
+
 customers = Customer.all()
+films = Film.all()
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -6,7 +6,22 @@ class Film
   def initialize(options)
     @id = options["id"].to_i if options["id"]
     @title = options["title"]
-    @price = options["price"]
+    @price = options["price"].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO films (title, price)
+          VALUES ($1, $2)
+          RETURNING id"
+    values = [@title, @price]
+    result = SqlRunner.run(sql, values)
+    @id = result.first["id"].to_i
+  end
+
+  def Film.all()
+    sql = "SELECT * FROM films"
+    films_hashes = SqlRunner.run(sql)
+    return films_hashes.map {|film_hash| Film.new(film_hash)}
   end
 
 
